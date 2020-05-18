0cf5697fdb41d277184b547a303e9b9676373880
AbderrahmenBmz <abdogray@gmail.com>
2020-05-18 12:08:02 +0100

    add registration screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/screens/login_screen.dart';
+import 'package:chatapp/screens/registration_screen.dart';
 import 'package:chatapp/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.amber,
       ),
-      home: LoginScreen(),
+      home: RegistrationScreen()
     );
   }
 }
