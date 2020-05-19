09d66db0dd0917e7474432ea36505250fa9b4cce
AbderrahmenBmz <abdogray@gmail.com>
2020-05-19 10:26:02 +0100

    add login and logout

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,14 +1,20 @@
 import 'package:chatapp/components/rounded_bottun.dart';
 import 'package:chatapp/constants.dart';
+import 'package:chatapp/screens/chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,24 +36,39 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 30.0,
             ),
             TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kInputDecoration.copyWith(hintText: 'Enter your email')
-            ),
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your email')),
             SizedBox(
               height: 8.0,
             ),
             TextField(
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
-              decoration:  kInputDecoration.copyWith(hintText: 'Enter your password'),
+              decoration:
+                  kInputDecoration.copyWith(hintText: 'Enter your password'),
             ),
             SizedBox(
               height: 24.0,
             ),
-            RoundedButton(title:'Log In',color: Colors.amber,onPressed: (){}),
+            RoundedButton(
+              title: 'Log In',
+              color: Colors.amber,
+              onPressed: () async {
+                try {
+                  final user = await _auth.signInWithEmailAndPassword(
+                      email: email, password: password);
+                  if (user != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+            ),
           ],
         ),
       ),
